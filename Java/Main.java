0152f8bfee56ccca09726892e4e93f88fede66ae
Jose Pozo Silva <jhosede23@gmail.com>
2022-07-30 01:37:05 -0400

    updating files java, Python - creating obj and class

--- Java/Main.java
+++ Java/Main.java
@@ -5,6 +5,20 @@ class Main{
     public static void main(String [] args ) {
 
         System.out.println("Hola Mundop");
+
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andrea";
+        car.passegenger = 4;
+       /// System.out.println("Car License :"+car.license);
+        car.printDataCar();
+
+        Car car2 =new Car();
+        car2.license = "QWS6565";
+        car2.driver = "carla";
+        car2.passegenger = 6;
+       // System.out.println("Car License: "+car2.license);
+       car2.printDataCar();
         
     }
 
--- Python/Main.py
+++ Python/Main.py
@@ -1,2 +1,13 @@
+from Car import Car
 if __name__ == "__main__":
     print("Hola Mundo")
+    
+    car = Car()
+    car.license = "AMS123"
+    car.driver = "Camila"
+    print(vars(car))
+    
+    car2 = Car()
+    car2.license = "SAD895"
+    car2.driver = "Sonia"
+    print(vars(car2))
